1309cd702d182c50c963e5f9cbd5391d8be1c9d2
Michael Hu <pxmyhdev@gmail.com>
2020-07-22 19:01:48 -0400

    use sns topic

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 [![Build Status](https://travis-ci.com/PXMYH/joker.svg?branch=master)](https://travis-ci.com/PXMYH/joker)
 
 Push Jokes via SMS in serverless fashion
+
+## Resources
+JD Cloud & AI Joke API: https://wx.jdcloud.com/market/datas/5/10914
--- joke.py
+++ joke.py
@@ -15,7 +15,7 @@ url = URL['chn']
 TYPE = 'CHN'
 TIME = datetime.date(datetime.now())
 PARAMS = {'time': TIME, 'page': '1', 'maxResult': '1', 'showapi_sign': 'bd0592992b4d4050bfc927fe7a4db9f3',
-          'appkey': os.environ['API_KEY']}
+          'appkey': os.environ['JD_API_KEY']}
 TPOIC_ARN = 'arn:aws:sns:us-east-1:683778474338:joke'
 
 
@@ -65,11 +65,5 @@ client = boto3.client(
     aws_secret_access_key=os.environ['AWS_ACCESS_SECRET'],
     region_name=os.environ['AWS_REGION']
 )
-
-# Send your sms message.
-# client.publish(
-#     PhoneNumber=os.environ['PHONE_NUMBER'],
-#     Message=f"{joke}"
-# )
 
 client.publish(TopicArn=TPOIC_ARN, Message=f"{joke}")
